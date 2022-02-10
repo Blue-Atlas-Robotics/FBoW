4ddf5722520a8d286793e73c86dd1f20e72abb41
Lionel Burg <lionel@blueatlasrobotics.com>
2022-02-10 11:32:15 +0100

    add functionality to generate features from videos

--- utils/fbow_dump_features.cpp
+++ utils/fbow_dump_features.cpp
@@ -70,6 +70,73 @@ std::vector<cv::Mat> loadFeatures(const std::vector<std::string>& path_to_images
     return features;
 }
 
+std::vector<cv::Mat> loadFeaturesFromVideo(std::vector<std::string> video_file_paths, const std::string& descriptor = "orb") {
+    cv::Ptr<cv::Feature2D> feat_detector;
+    if (descriptor == "orb") feat_detector = cv::ORB::create(2000);
+    else if (descriptor == "brisk") feat_detector = cv::BRISK::create();
+    else if (descriptor == "akaze") feat_detector = cv::AKAZE::create(cv::AKAZE::DESCRIPTOR_MLDB, 0, 3, 1e-4);
+#ifdef USE_CONTRIB
+    else if (descriptor == "surf") feat_detector = cv::xfeatures2d::SURF::create(15, 4, 2);
+#endif
+    else throw std::runtime_error("invalid descriptor: " + descriptor);
+
+    assert(!descriptor.empty());
+    std::vector<cv::Mat> features;
+
+    std::cout << "extracting features ..." << std::endl;
+    cv::VideoCapture cap;
+    int i = 0;
+    cv::Mat frame;
+    
+    for (std::string video_file_path : video_file_paths) {
+        cap.open(video_file_path);
+        if(!cap.isOpened()){
+            std::cout << "Error opening video stream or file" << std::endl;
+            return features;
+        }
+        i = 0;
+        while(1){
+            std::vector<cv::KeyPoint> keypoints;
+            cv::Mat descriptors;
+            std::cout << "reading frame: #" << i << std::endl;
+            cap >> frame;
+            if (frame.empty()) {
+                std::cerr << "could not open frame: #" << i << ". Aborting." << std::endl;
+                break;
+            }
+            feat_detector->detectAndCompute(frame, cv::Mat(), keypoints, descriptors);
+            std::cout << "extracted features: total = " << keypoints.size() << std::endl;
+            features.push_back(descriptors);
+            i++;
+        }
+    }
+    cap.release();
+    std::cout << "done detecting features" << std::endl;
+    return features;
+}
+
+std::vector<std::string> createVideoList(std::string name_file){
+    std::vector<std::string> video_paths;
+    try
+    {
+        std::ifstream file(name_file);
+        std::string video_path; 
+        while (std::getline(file, video_path))
+        {
+         video_paths.push_back(video_path);
+        }
+        return video_paths;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return video_paths;
+    }
+    
+    
+    
+}
+
 void saveToFile(const std::string& filename, const std::vector<cv::Mat>& features, std::string desc_name, bool rewrite = true) {
     if (!rewrite) {
         std::fstream ifile(filename, std::ios::binary);
@@ -109,7 +176,7 @@ int main(int argc, char** argv) {
     try {
         CmdLineParser cml(argc, argv);
         if (cml["-h"] || argc < 4) {
-            std::cerr << "Usage: DESCRIPTOR_NAME (= orb, brisk, akaze, surf(contrib)) FEATURE_OUTPUT IMAGES_DIR" << std::endl;
+            std::cerr << "Usage: DESCRIPTOR_NAME (= orb, brisk, akaze, surf(contrib)) FEATURE_OUTPUT IMAGES_DIR USING_VIDEO_BOOLEAN(TRUE,FALSE)-> USE VIDEO_NAME AS THIRD ARGUMENT" << std::endl;
             std::cerr << std::endl;
             std::cerr << "First step of creating a vocabulary is extracting features from a set of images." << std::endl;
             std::cerr << "We save them to a file for next step." << std::endl;
@@ -120,8 +187,14 @@ int main(int argc, char** argv) {
         const std::string descriptor = argv[1];
         const std::string output = argv[2];
 
-        auto images = DirReader::read(argv[3]);
-        std::vector<cv::Mat> features = loadFeatures(images, descriptor);
+        std::vector<cv::Mat> features;
+        if(argc < 5){
+            auto images = DirReader::read(argv[3]);
+            features = loadFeatures(images, descriptor);
+        }else if (argv[4] == std::string("TRUE")){
+            std::vector<std::string> video_path_list =createVideoList(argv[3]);
+            features = loadFeaturesFromVideo(video_path_list, descriptor);
+        }
 
         std::cout << "saving the features: " << argv[2] << std::endl;
 
